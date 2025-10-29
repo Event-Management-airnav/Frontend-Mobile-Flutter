446404a5c2c7b6fdda924714a1448e064d511efe
Hidayat <75840967+hkvil@users.noreply.github.com>
2025-10-29 11:26:48 +0700

    initiate routing for dashboard, profile, auth. change to java 8

---
+++ lib/modules/participant/profile/profile_binding.dart
@@ -0,0 +1,9 @@
+import 'package:frontend_mobile_flutter/modules/participant/profile/profile_controller.dart';
+import 'package:get/get.dart';
+
+class ProfileBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<ProfileController>(() => ProfileController());
+  }
+}
