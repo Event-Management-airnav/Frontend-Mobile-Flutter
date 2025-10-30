39215fb380120c56c1512cd6a31ad0ebf022d2c3
doandadr <ddoanda@gmail.com>
2025-10-31 06:17:53 +0700

    add network layer files

---
+++ lib/data/network/endpoints.dart
@@ -0,0 +1,22 @@
+import 'package:frontend_mobile_flutter/env/env.dart';
+
+class Endpoints {
+  static final String baseUrl = Env.baseUrl;
+
+
+  // Auth
+  static final register = "$baseUrl/register";
+  static final login = "$baseUrl/login";
+  static final logout = "$baseUrl/logout";
+
+  // OTP
+  static final verifyOtp = "$baseUrl/verify-otp";
+  static final resendOtp = "$baseUrl/resend-otp";
+
+  // Password
+  static final forgotPassword = "$baseUrl/forgot-password";
+  static final resetPassword = "$baseUrl/reset-password";
+
+  // Profile
+  static final profile = "$baseUrl/profile";
+}
