18d1347f3aed963aed61a03369a3b0878f30e604
doandadr <ddoanda@gmail.com>
2025-11-01 00:23:35 +0700

    add dashboard data layer

---
+++ lib/data/models/profile/change_password_request.dart
@@ -0,0 +1,31 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:equatable/equatable.dart';
+
+part 'change_password_request.g.dart';
+
+@JsonSerializable()
+class ChangePasswordRequest extends Equatable {
+  @JsonKey(name: "current_password")
+  final String currentPassword;
+
+  @JsonKey(name: "new_password")
+  final String newPassword;
+
+  @JsonKey(name: "new_password_confirmation")
+  final String newPasswordConfirmation;
+
+  const ChangePasswordRequest({
+    required this.currentPassword,
+    required this.newPassword,
+    required this.newPasswordConfirmation,
+  });
+
+  factory ChangePasswordRequest.fromJson(Map<String, dynamic> json) =>
+      _$ChangePasswordRequestFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ChangePasswordRequestToJson(this);
+
+  @override
+  List<Object?> get props =>
+      [currentPassword, newPassword, newPasswordConfirmation];
+}
