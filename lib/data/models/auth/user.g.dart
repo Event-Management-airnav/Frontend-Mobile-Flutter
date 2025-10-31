8be470f0913db9c7825092eadb1ba7ef8f22e204
doandadr <ddoanda@gmail.com>
2025-10-31 07:51:47 +0700

    add login models and update auth_service

--- lib/data/models/auth/user.g.dart
+++ lib/data/models/auth/user.g.dart
@@ -13,6 +13,7 @@ User _$UserFromJson(Map<String, dynamic> json) => User(
   email: json['email'] as String,
   telp: json['telp'] as String,
   role: json['role'] as String,
+  emailVerifiedAt: json['email_verified_at'] as String?,
 );
 
 Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
@@ -22,4 +23,5 @@ Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
   'email': instance.email,
   'telp': instance.telp,
   'role': instance.role,
+  'email_verified_at': instance.emailVerifiedAt,
 };
