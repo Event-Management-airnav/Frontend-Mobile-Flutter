ce1fab7c20f3559ad15aa70966eb468a97bcb517
Hidayat <75840967+hkvil@users.noreply.github.com>
2025-11-01 10:25:47 +0700

    setup API for Activity Page

---
+++ lib/data/models/event/presence.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'presence.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Presence _$PresenceFromJson(Map<String, dynamic> json) =>
+    Presence(kode: json['kode'] as String);
+
+Map<String, dynamic> _$PresenceToJson(Presence instance) => <String, dynamic>{
+  'kode': instance.kode,
+};
