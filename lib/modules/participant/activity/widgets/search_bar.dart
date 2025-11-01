03286a24ab3e376fedc9d9b275673505e838d5e9
Hidayat <75840967+hkvil@users.noreply.github.com>
2025-11-01 12:05:07 +0700

    Integrate API for Activity Page (list of followed events by user), Scan and Cert is not done yet

--- lib/modules/participant/activity/widgets/search_bar.dart
+++ lib/modules/participant/activity/widgets/search_bar.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:frontend_mobile_flutter/modules/participant/activity/widgets/filter_button.dart';
+import 'package:get/get.dart';
+import '../activity_controller.dart';
 
 class TSearchBar extends StatelessWidget {
   const TSearchBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final c = Get.find<ActivityController>();
+
     return Column(
       children: [
-        //Search
         TextField(
           decoration: InputDecoration(
             hintText: 'Aktivitas....',
@@ -17,26 +20,40 @@ class TSearchBar extends StatelessWidget {
               borderRadius: BorderRadius.circular(40.0),
             ),
           ),
-          onChanged: (value) {
-            // Handle search input changes
-          },
+          onChanged: (value) {},
         ),
-        SizedBox(height: 10),
-        //Filter
-        Row(
-          children: [
-            FilterButton(
-              icon: Icon(Icons.check_circle),
-              onPressed: () {},
-              text: 'Selesai',
-            ),
-            SizedBox(width: 10),
-            FilterButton(
-                onPressed: () {},
-                icon: Icon(Icons.check_circle_outline),
-                text: 'On Going'),
-          ],
-        )
+        const SizedBox(height: 10),
+
+        // FILTER row
+        Obx(() {
+          final selected = c.selectedFilter.value;
+
+          return Row(
+            children: [
+              FilterButton(
+                icon: Icon(
+                  Icons.check_circle,
+                  color: selected == ActivityFilter.selesai ? Colors.white : Colors.black,
+                ),
+                text: 'Selesai',
+                onPressed: () => c.selectFilter(ActivityFilter.selesai),
+                isSelected: selected == ActivityFilter.selesai,
+              ),
+
+              const SizedBox(width: 10),
+
+              FilterButton(
+                icon: Icon(
+                  Icons.pending,
+                  color: selected == ActivityFilter.berlangsung ? Colors.white : Colors.black,
+                ),
+                text: 'Berlangsung',
+                onPressed: () => c.selectFilter(ActivityFilter.berlangsung),
+                isSelected: selected == ActivityFilter.berlangsung,
+              ),
+            ],
+          );
+        }),
       ],
     );
   }
