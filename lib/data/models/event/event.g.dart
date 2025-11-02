f360cec0ca1d0c4a70065dfba6da59b1cf64fe81
doandadr <ddoanda@gmail.com>
2025-11-02 23:58:27 +0700

    chore change json_serialized generated model files from crlf to lf format

--- lib/data/models/event/event.g.dart
+++ lib/data/models/event/event.g.dart
@@ -12,14 +12,13 @@ Event _$EventFromJson(Map<String, dynamic> json) => Event(
   nama: json['nama'] as String,
   tipe: json['tipe'] as String,
   lokasi: json['lokasi'] as String?,
-  tanggalMulai: json['tanggal_mulai'] as String?,
-  tanggalMulaiRaw: json['tanggal_mulai_raw'] as String?,
-  statusAcara: json['status_acara'] as String?,
-  statusEvent: json['status_event'] as String?,
+  tanggalMulai: json['tanggal_mulai'] as String,
+  tanggalMulaiRaw: json['tanggal_mulai_raw'] as String,
+  statusAcara: json['status_acara'] as String,
+  statusEvent: json['status_event'] as String,
   banner: json['banner'] as String?,
-  deskripsiSingkat: json['deskripsi_singkat'] as String?,
-  mdlKode: json['mdl_kode'] as String?,
-  mdlPresensiAktif: (json['mdl_presensi_aktif'] as num?)?.toInt(),
+  deskripsiSingkat: json['deskripsi_singkat'] as String,
+  mdlPresensiAktif: (json['mdl_presensi_aktif'] as num).toInt(),
 );
 
 Map<String, dynamic> _$EventToJson(Event instance) => <String, dynamic>{
@@ -34,6 +33,5 @@ Map<String, dynamic> _$EventToJson(Event instance) => <String, dynamic>{
   'status_event': instance.statusEvent,
   'banner': instance.banner,
   'deskripsi_singkat': instance.deskripsiSingkat,
-  'mdl_kode': instance.mdlKode,
   'mdl_presensi_aktif': instance.mdlPresensiAktif,
 };
