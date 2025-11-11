c1fb3a51c660a74afedf22626ef813c1946f73e9
Hidayat <75840967+hkvil@users.noreply.github.com>
2025-11-11 16:34:35 +0700

    disable snackbar certificate

--- lib/data/network/api_interceptor.dart
+++ lib/data/network/api_interceptor.dart
@@ -37,6 +37,8 @@ class ApiInterceptor extends Interceptor {
               backgroundColor: Colors.green);
           break;
         case 400:
+          //Ini disable snackbar sertifikat
+          if (response.data["message"] == "Sertifikat belum terbit" || response.data["message"] == "Absen tidak lengkap." ) break;
           Get.snackbar("Permintaan Tidak Valid",
               response.data["message"] ?? "Permintaan yang Anda kirim tidak valid",
               backgroundColor: failColor);
